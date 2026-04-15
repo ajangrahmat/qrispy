b991c9c6447b856fbd3cb43a06b08351814f164f
Ajang Rahmat <200890191+ArduLite@users.noreply.github.com>
2026-04-15 20:36:13 +0700

    Bump version to 0.1.1

--- qrispy/__init__.py
+++ qrispy/__init__.py
@@ -2,5 +2,5 @@ from .client import Mayar
 from .exceptions import QrispyError
 from .response import APIResponse
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["Mayar", "QrispyError", "APIResponse"]
